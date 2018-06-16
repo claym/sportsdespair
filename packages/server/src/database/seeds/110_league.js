d1cba45ea0285073c7f3ff06bb42a97cc4dc6fad
Clay Mitchell <clay@pfd.net>
2018-06-16 10:39:22 -0400

    removed logging

--- packages/server/src/database/seeds/110_league.js
+++ packages/server/src/database/seeds/110_league.js
@@ -1,7 +1,6 @@
 import { returnId } from '../../sql/helpers';
 
 export async function seed(knex) {
-  console.log('Loading Leagues');
   var data = require('../data/leagues.json');
   var sports = await knex('sport')
     .select('id', 'slug')
